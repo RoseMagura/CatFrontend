9c8417824f65204f08570978464b357417c45130
RoseMagura <magurarm@guilford.edu>
2021-02-23 20:46:39 -0500

    Feat: Add basic functionality

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 class Home extends React.Component {
     state = {
-        images: [],
+        allImages: [],
+        selectedImages: [],
     };
+
+    componentDidMount() {
+        this.fetchAll();
+    }
+
     fetchAll = () => {
         const apiUrl = 'http://localhost:3000/all';
         let imageList = [];
@@ -13,28 +19,47 @@ class Home extends React.Component {
                 data.forEach((element) => {
                     imageList.push(element);
                 });
-                this.setState({ images: imageList });
-            });
+                this.setState({ allImages: imageList });
+            })
+            .catch((error) => console.error(error));
     };
 
-    fetchById = () => {
-        let id = 5;
+    displayAll = () => {
+        this.setState({ selectedImages: this.state.allImages });
+    };
+
+    fetchById = (event) => {
+        const id = event.target.value;
+        // let id = 5;
         const apiUrl = `http://localhost:3000/id/${id}`;
         fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => {
-                this.setState({ images: data });
+                this.setState({ selectedImages: data });
             });
     };
     render() {
         return (
             <div>
                 <h1>Cat Pictures</h1>
-                <button onClick={this.fetchAll}>See All</button>
-                <button onClick={this.fetchById}>Pick By Id</button>
-                {this.state.images.map((image) => (
-                    <img key={image.id} src={image.url} alt={image.title} />
-                ))}
+                <button onClick={this.displayAll}>See All</button>
+                <div>
+                    <h2>Pick By Title:</h2>
+                    <select onChange={this.fetchById}>
+                        <option>Select a title</option>
+                        {this.state.allImages.map((i) => (
+                            <option value={i.id} key={i.id}>
+                                {i.title}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div id="image-grid">
+                    {this.state.selectedImages.map((image) => (
+                        <img key={image.id} src={image.url} alt={image.title} />
+                    ))}
+                </div>
+
             </div>
         );
     }
